eed0dc6b4fde0ac271bceb1206993a9e09743265
Aadrizeta <adrianzambranomontero@gmail.com>
2026-01-31 19:33:20 +0100

    hero section terminada

--- src/components/layout/hero-section/hero-section.tsx
+++ src/components/layout/hero-section/hero-section.tsx
@@ -1,7 +1,21 @@
 import '@/app/globals.css';
+import Image from 'next/image';
+import MainButton from '@/components/ui/hero-section/main-button';
 
 export default function HeroSection() {
     return (
-        <section className="bg-soria-red min-h-[75vh] flex flex-col items-center justify-center text-center overflow-hidden"></section>
+        <section className="col-center bg-soria-red min-h-[75vh] gap-15 text-center overflow-hidden">
+            <div className='col-center'>
+                <h1>
+                    <Image src="/logos/Logo-h1.png" alt="La Sorianita - Valor Artesano" width={600} height={300} />
+                </h1>
+                <div className="w-full flex justify-center items-center gap-4 opacity-80">
+                    <div className="side-bars"></div>
+                    <p className='text-sm font-light tracking-wider text-white text-inter'>MADRID · CARFT · BAKERY</p>
+                    <div className="side-bars"></div>
+                </div>
+            </div>
+            <MainButton />
+        </section>
     );
 }
---
+++ src/components/ui/hero-section/main-button.tsx
@@ -0,0 +1,10 @@
+import '@/app/globals.css';
+import Link from "next/link";
+
+export default function MainButton() {
+    return (
+        <Link href="href">
+            <button className="main-button">VEN A DESCUBRIRNOS</button>
+        </Link>
+    );
+}
