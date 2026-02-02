c362a7f84e70c2476427fd6847218aedbd39f510
Adrian <adrianzambranomontero@gmail.com>
2026-02-02 11:59:32 +0100

    nuevo workflow despliegue

--- src/components/layout/about-us/legado-section.tsx
+++ src/components/layout/about-us/legado-section.tsx
@@ -11,7 +11,7 @@ export default function Legado() {
                         <span className='ml-12 md:ml-20'>Artesano</span>
                     </h2>
                     <p className='legado-text'>Desde 1955, en La Sorianita hemos mantenido intacta la tradición panadera. Cada mañana, nuestras manos amasan con el mismo cariño que lo hacían nuestros abuelos.</p>
-                    <p className='legado-text'>No utilizamos atajos. Fermentaciones lentas, masa madre cultivada por décadas y el respeto absoluto por el tiempo. Porque las cosas buenas no se pueden apresurar</p>
+                    <p className='legado-text'>No utilizamos atajos. Fermentaciones lentas, masa madre cultivada por décadas y el respeto absoluto por el tiempo. Porque las cosas buenas no se pueden apresurar NUEVO</p>
                     <p className='w-full text-start text-soria-red text-4xl md:text-5xl font-script'>Mestros pasteleros</p>
                 </div>
                 <Wallpaper />
