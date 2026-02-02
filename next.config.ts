4851ebdcd24f7441c94107bbfb19bb4988030d61
Adrian <adrianzambranomontero@gmail.com>
2026-02-02 14:33:45 +0100

    experimental css

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  experimental: {
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
