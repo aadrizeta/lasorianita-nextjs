2d7167dc805e1eadb02e45d7c86c9633cdfd41b9
Adrian <adrianzambranomontero@gmail.com>
2026-02-12 10:32:29 +0100

    numero corregido

--- src/app/encuentranos/page.tsx
+++ src/app/encuentranos/page.tsx
@@ -3,7 +3,7 @@ import Location from "@/components/layout/location-section/location-section"
 function Encuentranos() {
   return (
     <div className="mt-20">
-      <p className="h-screen p-50 text-center font-math text-4xl">Coming Soon...</p>
+      <p className="h-screen py-20 text-center font-math text-4xl">Coming Soon...</p>
       {/* <Location /> */}
     </div>
   )
--- src/components/ui/footer/MainFooter.tsx
+++ src/components/ui/footer/MainFooter.tsx
@@ -1,27 +1,27 @@
 export default function MainFooter() {
-    return (
-        <div className='padding-responsive col-center gap-20 md:gap-10 md:flex-row md:justify-between lg:items-stretch py-10'>
-            <div className='col-center text-center lg:text-start'>
-                <span className='font-serif text-4xl mb-6 leading-tight text-soria-cream'>Endulzando tu día desde</span>
-                <div className="flex flex-row gap-5 items-end justify-center">
-                    <span className='text-9xl md:text-[10rem] leading-none block -mb-8 md:-mb-12 text-soria-cream font-script opacity-90 select-none'>1955</span>
-                    <span className="md:text-2xl md:top-6 md:right-46 text-xl text-soria-cream tracking-[0.3em] font-math right-4 pt-10">EST.</span>
-                </div>
-            </div>
-            <div className='w-full h-45.25 max-w-md flex flex-col justify-between gap-5'>
-                <span className="font-serif text-4xl leading-tight text-soria-cream text-center lg:text-start">Contáctanos</span>
-                <div className='flex flex-row justify-between'>
-                    <div className='flex flex-col font-sans text-soria-cream'>
-                        <span className="md:text-lg">TELÉFONO</span>
-                        <a href="tel:+34605467989" className="md:text-xl hover:text-white transition-colors">+34 628 15 06 92</a>
-                    </div>
-                    <div className='flex flex-col font-sans text-soria-cream'>
-                        <span className="md:text-lg">EMAIL</span>
-                        <a href="mailto:info@lasorianita.es" className="md:text-xl hover:text-white transition-colors" target="_blank">info@lasorianita.es</a>
-                    </div>
-                </div>
-                <span className='text-xs text-soria-cream font-sans flex justify-between'>© 2024 LA SORIANITA | OBRADOR ARTESANAL</span>
-            </div>
+  return (
+    <div className='padding-responsive col-center gap-20 md:gap-10 md:flex-row md:justify-between lg:items-stretch py-10'>
+      <div className='col-center text-center lg:text-start'>
+        <span className='font-serif text-4xl mb-6 leading-tight text-soria-cream'>Endulzando tu día desde</span>
+        <div className="flex flex-row gap-5 items-end justify-center">
+          <span className='text-9xl md:text-[10rem] leading-none block -mb-8 md:-mb-12 text-soria-cream font-script opacity-90 select-none'>1955</span>
+          <span className="md:text-2xl md:top-6 md:right-46 text-xl text-soria-cream tracking-[0.3em] font-math right-4 pt-10">EST.</span>
         </div>
-    );
+      </div>
+      <div className='w-full h-45.25 max-w-md flex flex-col justify-between gap-5'>
+        <span className="font-serif text-4xl leading-tight text-soria-cream text-center lg:text-start">Contáctanos</span>
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-col font-sans text-soria-cream'>
+            <span className="md:text-lg">TELÉFONO</span>
+            <a href="tel:+34628150692" className="md:text-xl hover:text-white transition-colors">+34 628 15 06 92</a>
+          </div>
+          <div className='flex flex-col font-sans text-soria-cream'>
+            <span className="md:text-lg">EMAIL</span>
+            <a href="mailto:info@lasorianita.es" className="md:text-xl hover:text-white transition-colors" target="_blank">info@lasorianita.es</a>
+          </div>
+        </div>
+        <span className='text-xs text-soria-cream font-sans flex justify-between'>© 2024 LA SORIANITA | OBRADOR ARTESANAL</span>
+      </div>
+    </div>
+  );
 }
