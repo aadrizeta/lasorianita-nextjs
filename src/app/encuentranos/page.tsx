030892e36a6fde6a9c06b9ad4783c6979d570ba7
Adrian <adrianzambranomontero@gmail.com>
2026-02-13 14:29:55 +0100

    encuentranos modificado

--- src/app/encuentranos/page.tsx
+++ src/app/encuentranos/page.tsx
@@ -1,23 +1,25 @@
 import Location from "@/components/layout/location-section/location-section"
 import Image from "next/image"
+import ScrollStrip from "@/components/ui/scroll-main/scroll-strip"
 
 function Encuentranos() {
   return (
     <>
-      <div className="mt-20 relative w-full aspect-video max-h-[60vh]">
+      <div className="mt-20 relative w-full aspect-video max-h-[70vh]">
         <div
           className={`absolute inset-0 transition-opacity duration-700 ease-in-out`}
         >
           <Image
-            src="/images/banner-mostrador.webp"
+            src="/images/tarta-de-frutas.webp"
             alt="Banner mostrador"
             fill
-            className="object-cover object-center"
+            className="object-cover object-[50%_75%]"
             sizes="100vw"
             quality={85}
           />
         </div>
       </div>
+      <ScrollStrip />
       <Location />
     </>
   )
